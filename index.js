02f6ec52bf0bd638c1f063174025d0df2fff7229
Tobias Baunbæk Christensen <freeall@gmail.com>
2017-01-11 11:24:14 +0100

    cache files

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var express = require('express')
 var path = require('path')
+var serveStatic = require('serve-static')
 
 var app = express()
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'))
 })
-app.use('/en', express.static(path.join(__dirname, '/en')))
-app.use('/es', express.static(path.join(__dirname, '/es')))
+app.use('/en', serveStatic(path.join(__dirname, '/en'), { maxAge: '1y' }))
+app.use('/es', serveStatic(path.join(__dirname, '/es'), { maxAge: '1y' }))
 
 app.listen(process.env.PORT || 9933, () => {
   console.log('listening on ' + (process.env.PORT || 9933));
